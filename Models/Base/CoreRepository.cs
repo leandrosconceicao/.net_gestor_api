322565495089cf0351026da405b81282e38c98b1
Leandro <leandrosantosconceicao@gmail.com>
2024-08-08 22:37:48 -0300

    Refatorações

--- Models/Base/CoreRepository.cs
+++ Models/Base/CoreRepository.cs
@@ -1,5 +1,6 @@
 ﻿
 using Api.Data;
+using Api.Data.Dtos.AccountDtos;
 using Api.Data.Dtos.EstablishmentDtos;
 using Api.Data.Dtos.UserDtos;
 using AutoMapper;
@@ -10,12 +11,11 @@ namespace Api.Models.Base
     public class CoreRepository : ICoreRepository
     {
         private readonly ApiContext _context;
-        private readonly IMapper _mapper;
 
-        public CoreRepository(ApiContext context, IMapper mapper)
+        public CoreRepository(ApiContext context)
         {
             _context = context;
-            _mapper = mapper;
+
         }
 
         public void Add<T>(T entity) where T : class
@@ -38,35 +38,59 @@ namespace Api.Models.Base
             _context.Update(entity);
         }
 
-        public async Task<ReadUserDto?> FindUserById(int id)
+        public async Task<User?> FindUserById(int id)
         {
             User? query = await _context.Users
                 .Include(user => user.Establishment)
                 .FirstOrDefaultAsync(user => user.Id == id);
-            return query == null ? null : _mapper.Map<ReadUserDto>(query);
+            return query;
         }
 
-        public async Task<IEnumerable<ReadUserDto>> FindAllUsers(int offset, int limit)
+        public async Task<IEnumerable<User>> FindAllUsers(int offset, int limit)
         {
             IEnumerable<User> query = await _context.Users
                 .Skip(offset)
                 .Take(limit)
-                .Include(user => user.Establishment)
+                // .Include(user => user.Establishment)
                 .ToListAsync();
-            return _mapper.Map<List<ReadUserDto>>(query);
+            return query;
         }
 
-        public async Task<ReadEstablishmentDto?> FindEstablishmentById(int id)
+        public async Task<Establishment?> FindEstablishmentById(int id)
         {
             Establishment? query = await _context.Establishments
                 .FirstOrDefaultAsync(est => est.Id == id);
-            return query == null ? null : _mapper.Map<ReadEstablishmentDto>(query);
+            return query;
         }
 
-        public async Task<IEnumerable<ReadEstablishmentDto>> FindAllEstablishments(int offset = 0, int limit = 50)
+        public async Task<IEnumerable<Establishment>> FindAllEstablishments(int offset = 0, int limit = 50)
         {
-            List<Establishment> query = await _context.Establishments.ToListAsync();
-            return _mapper.Map<IEnumerable<ReadEstablishmentDto>>(query);
+            List<Establishment> query = await _context.Establishments
+                .Skip(offset)
+                .Take(limit)
+                .ToListAsync();
+            return query;
+        }
+
+        public async Task<Account?> FindAccountById(int id)
+        {
+            var query = await _context.Accounts
+                .Include(acc => acc.Client)
+                .Include(acc => acc.Client.Addresses)
+                .Include(acc => acc.Establishment)
+                .Include(acc => acc.User)
+                .FirstOrDefaultAsync(acc => acc.Id == id);
+            return query;
+        }
+
+        public async Task<IEnumerable<Account>> FindAllAccounts(int establishmentId, int offset = 0, int limit = 50)
+        {
+            var query = await _context.Accounts
+                .Skip(offset)
+                .Take(limit)
+                .Where(account => account.EstablishmentId == establishmentId)
+                .ToListAsync();
+            return query;
         }
     }
 }
