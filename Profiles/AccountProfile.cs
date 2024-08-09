63097d1c21d04f1b28a64df4703f0e17fe9a10a8
Leandro <leandrosantosconceicao@gmail.com>
2024-08-08 22:36:34 -0300

    Profile de mapeamento do modelo de account

---
+++ Profiles/AccountProfile.cs
@@ -0,0 +1,15 @@
+﻿using Api.Data.Dtos.AccountDtos;
+using Api.Models;
+using AutoMapper;
+
+namespace Api.Profiles;
+
+public class AccountProfile : Profile
+{
+    public AccountProfile()
+    {
+        CreateMap<AccountDto, Account>();
+        CreateMap<Account, AccountDto>();
+        CreateMap<UpdateAccountDto, Account>();
+    }
+}
