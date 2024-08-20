c163111c6883f23dc36bea2caac5f56bc42b30eb
Leandro <leandrosantosconceicao@gmail.com>
2024-08-20 17:43:34 -0300

    Novas propriedades

--- Gestor.Helpers/Models/EstablishmentDtos/ReadEstablishmentDto.cs
+++ Gestor.Helpers/Models/EstablishmentDtos/ReadEstablishmentDto.cs
@@ -22,5 +22,9 @@
         public string Email { get; set; } = string.Empty;
         public string Phone { get; set; } = string.Empty;
 
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
     }
 }
