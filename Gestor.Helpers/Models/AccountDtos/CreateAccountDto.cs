5c41d76cb166f04dc05a00f47cffd27333cd433f
Leandro <leandrosantosconceicao@gmail.com>
2024-08-20 17:42:52 -0300

    Novos DTOS

---
+++ Gestor.Helpers/Models/AccountDtos/CreateAccountDto.cs
@@ -0,0 +1,18 @@
+﻿using Gestor.Domain.Entities;
+using Gestor.Helpers.Models.ClientDtos;
+
+namespace Gestor.Helpers.Models.AccountDtos
+{
+    public class CreateAccountDto
+    {
+        public string Description { get; set; }
+
+        public AccountStatus Status { get; set; } = AccountStatus.Open;
+
+        public int UserId { get; set; }
+
+        public int EstablishmentId { get; set; }
+
+        public CreateClientDto? Client { get; set; } = new CreateClientDto();
+    }
+}
---
+++ Gestor.Helpers/Models/ClientDtos/CreateClientDto.cs
@@ -0,0 +1,22 @@
+﻿using Gestor.Domain.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace Gestor.Helpers.Models.ClientDtos
+{
+    public class CreateClientDto
+    {
+
+        [StringLength(14, MinimumLength = 11, ErrorMessage = "Tamanho inválido")]
+        public string Cgc { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; } = string.Empty;
+
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        public ICollection<ClientAddress> Addresses { get; set; } = [];
+    }
+}
