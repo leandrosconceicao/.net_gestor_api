11047165165232dfc61a57613f57bd1fea527259
Leandro <leandrosantosconceicao@gmail.com>
2024-10-10 22:26:50 -0300

    Refatorando metodos

--- Gestor.Repository/ProductRepository.cs
+++ Gestor.Repository/ProductRepository.cs
@@ -2,6 +2,7 @@
 using Gestor.Domain.Entities;
 using Gestor.Repository.Db;
 using Gestor.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using MySql.Data.MySqlClient;
 
@@ -24,92 +25,25 @@ namespace Gestor.Repository
         public async Task<int> AddProductAsync(Product product)
         {
             _context.Products.Add(product);
-            return await _context.SaveChangesAsync();
-            //string query = @"INSERT INTO products (
-            //                 Name,
-            //                    IsActive,
-            //                    Price,
-            //                    RequirePreparation,
-            //                    ImagePath,
-            //                    CreatedAt,
-            //                    EstablishmentId,
-            //                    CategoryId
-            //                ) VALUES (
-            //                 @Name,
-            //                    @IsActive,
-            //                    @Price,
-            //                    @RequirePreparation,
-            //                    @ImagePath,
-            //                    NOW(),
-            //                    @EstablishmentId,
-            //                    @CategoryId
-            //                );
-            //                SELECT LAST_INSERT_ID();
-            //                INSERT INTO productxextras (
-            //                 Required,
-            //                 ProductExtraId,
-            //                 ProductId 
-            //                ) values (
-            //                 @Required,
-            //                 @ProductExtraId,
-            //                 LAST_INSERT_ID()
-            //                )";
-            //using var con = new MySqlConnection(_connectionString);
-            //int rowsAffectd = await con.ExecuteScalarAsync<int>(query, product);
-            //Console.WriteLine(query);
-            //return rowsAffectd;
+            await _context.SaveChangesAsync();
+            return product.Id;
         }
 
-        public Task<bool> DeleteProductAsync(int id)
+        public async Task<bool> DeleteProductAsync(Product product)
         {
-            throw new NotImplementedException();
+            _context.Products.Remove(product);
+            var rowsAffected = await _context.SaveChangesAsync();
+            return rowsAffected > 0;
         }
 
         public async Task<Product?> FindProductById(int id)
         {
-            using var con = new MySqlConnection(_connectionString);
-            string query = @"SELECT 
-	                            p.Id, 
-                                p.Name, 
-                                p.IsActive, 
-                                p.Price, 
-                                p.RequirePreparation, 
-                                p.ImagePath,
-                                p.CreatedAt,
-                                p.UpdatedAt,
-                                p.EstablishmentId,
-                                p.CategoryId,
-                                p.Deleted,
-                                pcat.Id,
-                                pcat.Description,
-                                pcat.ImagePath,
-                                pcat.Ordenation,
-                                pcat.CreatedAt,
-                                pcat.UpdatedAt,
-                                pcat.EstablishmentId,
-                                px.Id,
-                                px.Name,
-                                px.MaxQtdAllowed,
-                                px.EstablishmentId,
-                                px.Deleted,
-                                pxi.Id,
-                                pxi.Name,
-                                pxi.Price
-		                    FROM products p
-	                            inner join productcategories pcat on pcat.Id = p.CategoryId
-                                inner join productextras px on px.Id = pxe.ProductExtraId
-                                left join productextraitems pxi on pxi.ProductExtraId = px.Id
-	                        where p.Id = @Id
-                                group by pxe.Required, pxe.ProductExtraId";
-            var products = await con.QueryAsync<Product, ProductCategory, ProductCrossExtras, ProductExtraItem, Product>(query,
-                (product, productCategory, productExtra, ProductExtraItem) =>
-                    {
-                        product.Category = productCategory;
-                        product.ProductExtras.Add(productExtra);
-                        return product;
-                    }, new { Id = id }, splitOn: "EstablishmentId, Id");
-            var newProduct = products.First();
-            return newProduct;
+            return _context.Products
+                .Include(prod => prod.Category)
+                .Include(prod => prod.ProductExtras)
+                .ThenInclude(extras => extras.ProductExtra)
+                .ThenInclude(extraItem => extraItem.Items)
+                .FirstOrDefault(x => x.Id == id);
         }
 
         public async Task<IEnumerable<Product>> FindProductsAsync(int establishmentId, int limit = 50, int offset = 0)
@@ -122,6 +56,14 @@ namespace Gestor.Repository
             using var con = new MySqlConnection(_connectionString);
             var products = await con.QueryAsync<Product>(query, new { Id = establishmentId, Limit = limit, Offset = offset });
             return products;
+        }
+
+        public bool UpdateProductAsync(Product product)
+        {
+            _context.Products.Update(product);
+            var rows = _context.SaveChangesAsync();
+            rows.Wait();
+            return rows.Result > 0;
         }
     }
 }
